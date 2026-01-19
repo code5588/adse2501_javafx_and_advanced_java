41e4255e46dce26d512a78bffbbbe86a39cd7260
Nyanjui <nyanjuiarthur@gmail.com>
2026-01-19 10:36:48 +0300

    Add the initial HelloWorld Java class.

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Java program to display "Hello World!" on the console.
+ * 
+ * @author a.nyanjui
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+    
+}
